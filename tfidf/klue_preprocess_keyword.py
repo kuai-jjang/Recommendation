70569fdef089cbce63559a3b42ccd3b88a93833a
limaries30 <limaries30@gmail.com>
2019-09-23 00:35:41 +0900

    선수추가

--- tfidf/klue_preprocess_keyword.py
+++ tfidf/klue_preprocess_keyword.py
@@ -37,15 +37,17 @@ class prerpoces:
 
 if __name__=="__main__":
     
+    keyword_dict={1:'',2:'과제',3:'출석',4:'선수'}
+
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', type=str,required=True,
                 help='./data')
-    parser.add_argument('--keyword', type=str,required=True,
+    parser.add_argument('--keyword', type=int,required=True,
                 help="What keyword?")
 
     args = parser.parse_args()
     base_dir = args.dir
-    keyword = args.keyword
+    keyw = keyword_dict[args.keyword]
     data_dir=os.path.join(base_dir,'raw\*')
     save_dir=os.path.join(base_dir,'prep')
     files=glob.glob(data_dir)
@@ -55,16 +57,19 @@ if __name__=="__main__":
     print(komoran.morphs(u'졸린데 수업 끝내주세요'))
     print(komoran.nouns(u'모기 물렸다'))
     print(data_dir)
+
+    
     for i in files:
         df=pd.read_csv(i)
         df['trial']=df['LectureEval'].apply(lambda x:prerpoces(x).rem())
-        df['keyword_sent']=df['trial'].apply(lambda x:prerpoces(x).extract('과제'))
+        df['keyword_sent']=df['trial'].apply(lambda x:prerpoces(x).extract(keyw))
         df_extract=df[df.keyword_sent.str.len()!=0] #keyword없는 것 제외
         df_extract.drop(['trial'],axis=1,inplace=True)
 
         print(df.keyword_sent.head(5))
         semester=i.split('\\')[-1][:-4]
-        save_name='/'.join(i.split('\\')[:-2])+'/'+'prep'+'/'+semester+'_pre'
+        print(semester)
+        save_name='/'.join(i.split('\\')[:-2])+'/'+'prep'+'/'+semester+'_'+str(args.keyword)
         print(save_name)
         if not os.path.isfile(save_name):
             df_extract.to_csv(save_name,encoding='utf-8',index=False)
