7ba410b295993814511663fa0ff1c1fd6e8deb38
limaries30 <limaries30@gmail.com>
2019-11-28 18:09:24 +0900

    droplast

--- embedding/train.py
+++ embedding/train.py
@@ -120,7 +120,7 @@ if __name__=="__main__":
 
             running_loss+=loss.item()
             if i % 499 == 0:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.5f' %(epoch + 1, i + 1, running_loss / 50))
+                print('[%d, %5d] loss: %.5f' %(epoch + 1, i + 1, running_loss / 499))
                 running_loss = 0.0
 
             #torch.save(model.state_dict(),args.save_dir)
