2dd812e07efccd215ee08f248b5ac465d8d12464
limaries30 <limaries30@gmail.com>
2019-12-22 14:37:31 +0900

    tfidf

--- recommendation/tfidf/apply_tfidf.py
+++ recommendation/tfidf/apply_tfidf.py
@@ -8,6 +8,7 @@ import random
 import argparse
 import re
 import pickle
+from sklearn.decomposition import PCA
 from eunjeon import Mecab
 
 
@@ -43,15 +44,21 @@ class tf_idf:
         return tfidf,vocab
 
 class recommend:
-    def __init__(self,x,num,metric):
+    def __init__(self,x,idx,num,metric,data,pca_1=True):
         func_dict={'l2_dis':self.l2_dis,'cossim':self.cossim}
         self.x=x
         self.num=num
+        
+        self.idx=idx
         self.metric=func_dict[metric]
+        pca = PCA(n_components=10)
+        lec_vectors=data[data.columns[2:]].values if not pca_1 else pca.fit_transform(data[data.columns[2:]].values)
+        self.lec_vec=dict(zip(data[data.columns[0]],lec_vectors))
+ 
 
     def getit(self):   #사이킷런 knn 활용하기
-        my_vec=lec_vec[self.x]
-        new_dict=copy.deepcopy(lec_vec)  #깊복, 얕복 차이
+        my_vec=self.lec_vec[self.x]
+        new_dict=copy.deepcopy(self.lec_vec)  #깊복, 얕복 차이
         
         del new_dict[self.x]
         
@@ -65,7 +72,8 @@ class recommend:
                 q.append((k,dist))
                 q=collections.deque(sorted(q,reverse=True,key=lambda x:x[1]),maxlen=self.num)
             
-        return q
+        for idx,i in enumerate(q):
+            print(idx+1,'번째 가까운 강의',i[0])
 
     def l2_dis(self,x,y):
         return ((x-y)**2).sum()    
@@ -88,18 +96,18 @@ class preprocessing:
         sample_sentence=list(map(lambda x:re.sub('\n','',x),sample_sentence))
         sample_sentence=list(map(lambda x:tokenizer.pos(x),sample_sentence))
         token=sample_sentence
-
+    
         for idx,i in enumerate(sample_sentence):
             tokenized=list(filter(lambda x:x[1][0] not in remove_pos and x[1] not in remove_pos,i))
             tokenized=list(filter(lambda x:x[0] not in ['것','수'],tokenized))
-            token[idx]=[i for e in tokenized for i in e]
-        
-        return token
+            tokenized=list(map(lambda x:str(x),tokenized))
+            token[idx]=' '.join(tokenized)
+        return ' '.join(token)
 
     def prepro(self):
         
         self.idexes=[]
-        def removing(x):
+        def removed(x):
                        
             if len(x)!=0 and len(x)!=1:
                 
@@ -109,13 +117,11 @@ class preprocessing:
                 self.idexes.append(self.start_index)
                 self.start_index+=1
                 return None
+
         lecture_sentences=list(map(lambda x:ast.literal_eval(x),self.data))
         
-        lecture_sentences=list(filter(lambda x:removing(x),lecture_sentences)) #강의평이 없가나 1개 밖에 없는 경우 날림
-        
+        lecture_sentences=list(filter(lambda x:removed(x),lecture_sentences)) #강의평이 없가나 1개 밖에 없는 경우 날림
         lecture_sentences=list(map(lambda x:self.removing(x),lecture_sentences))
-        #lecture_sentences=[i for e in lecture_sentences for i in e]
-
         return lecture_sentences,self.idexes
 
 
@@ -125,6 +131,7 @@ if __name__=="__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--data_dir',default=r"C:\tensor_code\kluebot\data\raw\2018_2.csv", help='datadir?',type=str)
+    parser.add_argument('--classtype',default="교양", help='type?',type=str)
 
     args = parser.parse_args()
 
@@ -137,7 +144,10 @@ if __name__=="__main__":
 
     df=pd.read_csv(args.data_dir)
     
-    df_art=df[df.Classification=='교양']
+    df_art=df[df.Classification==args.classtype]
+    if args.classtype=='all':
+        df_art=df
+    
     df_start_idx=df_art.index.values[0]
 
     if 'raw' in args.data_dir:
@@ -149,32 +159,34 @@ if __name__=="__main__":
     M=preprocessing(df_art['keyword_sent'].values,tokenizer,remove_pos,df_start_idx)
     lecture_sentences,drop_idexes=M.prepro()
     
+    
 
     drop_list=df_art.loc[drop_idexes]
     df_art.drop(drop_idexes,inplace=True,axis=0)
 
+
+    df_art['pos_sent']=lecture_sentences
+
+
+
     df_art['doc_id']=list(zip(df_art['ProfessorName'],df_art['className']))
 
-    df_extract=pd.concat([df_art['doc_id'],df_art['keyword_sent']],axis=1)
+    df_extract=pd.concat([df_art['doc_id'],df_art['pos_sent']],axis=1).reset_index(drop=True)
     
-    print(df_extract.head())
 
-    
-    tfidf,vocab=tf_idf(df_extract['keyword_sent']).cal_tf()
+
+    tfidf,vocab=tf_idf(df_extract['pos_sent']).cal_tf()
   
-    data=pd.DataFrame(tfidf).T
-    print(data.head())
-
-    df_new=pd.concat([df_extract.reset_index(drop=True),data.reset_index(drop=True)],axis=1, ignore_index=True)
-    print(df_new.head())
+    data=pd.DataFrame(tfidf)
+ 
+    df_new=pd.concat([df_extract.reset_index(drop=True),data.reset_index(drop=True)],axis=1,ignore_index=True)
     df_new.to_csv('./lecture_vector_'+sem+'.csv',index=False)
-    lec_vec=dict(zip(df_extract['doc_id'],tfidf))
-
 
     # with open('./lec_vec_2017_1','wb') as f:
     #     pickle.dump(lec_vec,f)
-    # x=df_extract.className.iloc[random.randint(1,df_extract.shape[0])]
-    # print(x,'와 과제 관련 가장 가까운 강의들: ')
-    # print("l2: ",recommend(x,5,'l2_dis').getit())
-    # #print('\n')
-    # #print("코사인유사도:" ,recommend(x,5,cossim))
+    idx=random.randint(1,df_new.shape[0])
+    x=df_new[df_new.columns[0]].iloc[idx]
+    print(x,'와 과제 관련 가장 가까운 강의들: ')
+    recommend(x,idx,5,'l2_dis',df_new).getit()
+    #print('\n')
+    #print("코사인유사도:" ,recommend(x,5,cossim))
